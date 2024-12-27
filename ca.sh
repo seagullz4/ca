c8196f572bf270adaa9194f6dbd628788027c700
seagullz4 <96653729+seagullz4@users.noreply.github.com>
2024-12-27 12:57:45 +0800

    Update ca.sh

--- ca.sh
+++ ca.sh
@@ -3,8 +3,8 @@ generate_certificate() {
     domain_name=${user_domain:-"bing.com"}
     if curl --output /dev/null --silent --head --fail "$domain_name"; then
         openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout /etc/ssl/private/$domain_name.key -out /etc/ssl/private/$domain_name.crt -subj "/CN=$domain_name" -days 36500
-        chmod 777 /etc/ssl/private/$domain_name.key
-        chmod 777 /etc/ssl/private/$domain_name.crt
+        chmod +x /etc/ssl/private/$domain_name.key
+        chmod +x /etc/ssl/private/$domain_name.crt
         echo -e "${GREEN}自签名证书和私钥已生成！${NC}"
     else
         echo -e "${RED}无效的域名或域名不可用，请输入有效的域名！${NC}"
